c053794780615c7a2ca5f03e06eaa6f8b29b298e
Sonny Mayta <mayta.sonny@gmail.com>
2024-01-25 10:02:47 -0400

    Metodos parte teorica

---
+++ Metodos/Metodos/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Metodos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        /*
+         * Metodos son un grupo de instrucciones que realizan una tarea en concreto
+         * Sirven para hacer una tarea en un momento determinado
+         * Solo realiza su tarea hasta que es llamado
+         * Todos los métodos irán dentro de una clase
+         * Se debe especificar el tipo devuelto y los parámetros
+         * Los metodos y funciones hacen referencia a los mismo en C#
+         * 
+         * sintaxis
+         * 
+         * tipo nombre (parametros) {
+         *   cuerpo del método
+         *   return resultado;
+         * }
+         * 
+         * Existen métodos que no devuelvan algun valor
+         * Este tipo de metodo nunca llevará return
+         * 
+         * void nombre (parametros) {
+         *   cuerpo del método
+         */
+    }
+}
