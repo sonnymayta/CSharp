db2e6ef5311901b0f4470119f75def7e0e4033bd
Sonny Mayta <mayta.sonny@gmail.com>
2024-01-26 14:10:45 -0400

    Condicionales

---
+++ Condicionales/Condicionales/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Condicionales
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool haceFrio = true;
+            Console.WriteLine(haceFrio);
+
+            // Operador negación "!" invierte el valor de una variable booleana
+            Console.WriteLine(!haceFrio);
+
+            int edad = 18;
+
+            if (edad >= 18)
+            {
+                Console.WriteLine("La persona es mayor de edad.");
+            }
+        }
+
+        /*
+         * Operadores de comparación
+         * igual que ==
+         * diferente que !=
+         * menor que <
+         * menor o igual que <=
+         * mayor que >
+         * mayor o igual que >=
+         * 
+         * Operadores lógico
+         * Y lógico &&
+         * O lógico ||
+         */
+    }
+}
