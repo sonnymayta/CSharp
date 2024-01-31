fdda67a2db58b9693b5a99f442023253121a87b1
Sonny Mayta <mayta.sonny@gmail.com>
2024-01-30 22:58:34 -0400

    Excepciones parte teorica

---
+++ Excepciones/Excepciones/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Excepciones
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        /*
+         * EXCEPCIONES
+         * Las excepciones son errores en tiempo de ejecución del programa que escapan al control
+         * del programador.
+         * - memoria corrupta
+         * - desbordamiento de pila
+         * - sectores de disco dura defectuosos
+         * - conexiones de BBDD interrumpidas
+         * - etc.
+         * 
+         * Si intenta capturar el error y decir al programa que hacer cuando ocurre esa expeción
+         */
+    }
+}
