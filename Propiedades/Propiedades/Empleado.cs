7353698ce4de96cf786a718d89d51fb7998a6702
Sonny Mayta <mayta.sonny@gmail.com>
2024-02-10 18:31:52 -0400

    Propiedades o properties

---
+++ Propiedades/Propiedades/Empleado.cs
@@ -0,0 +1,49 @@
+﻿namespace Propiedades
+{
+    internal class Empleado
+    {
+        private double _salario;
+        private string nombre;
+
+        public Empleado(string nombre)
+        {
+            this.nombre = nombre;
+        }
+
+        //public void SetSalario(double salario)
+        //{
+        //    if (salario < 0)
+        //    {
+        //        Console.WriteLine("El salario no puede ser negativo. Se le asignara 0 como salario.");
+        //        this.salario = 0;
+        //    } 
+        //    else
+        //    {
+        //        this.salario = salario;
+        //    }
+        //}
+
+        //public double GetSalario()
+        //{
+        //    return salario;
+        //}
+
+        private double EvaluarSalario(double salario)
+        {
+            if (salario < 0) { return 0; } else { return salario; }
+        }
+
+        public double Salario
+        {
+            get => _salario;
+            set => _salario = EvaluarSalario(value);
+        }
+    }
+
+    /*
+     * PROPIEDADES (PROPERTIES)
+     * Se da el caso que no siempres se pueden usar las properties y en ocaciones la sintaxis es mas
+     * complicada que la encapsulación. Existen escenarios especificos donde las properties son más útiles que 
+     * el código convencional
+     */
+}
---
+++ Propiedades/Propiedades/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Propiedades
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Empleado Raul = new Empleado("Raul");
+            Raul.Salario = 1200;
+            Console.WriteLine("El salario del empleado es: {0}", Raul.Salario);
+            Raul.Salario += 500;
+            Console.WriteLine($"El salario del empleado es: {Raul.Salario}");
+        }
+    }
+}
