ce37f23dddb98eb948080060b62c13dc2dac7381
Sonny Mayta <mayta.sonny@gmail.com>
2024-02-10 11:33:53 -0400

    Clases abstractas

---
+++ ClasesAbstractas/ClasesAbstractas/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ClasesAbstractas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+    /*
+     * JERARQUÍA DE HERENCIA
+     * mamíferos (respirar(), pensar(), getNombre(), cuidarCrias())
+     * - ballena (nada())
+     * - caballo : IMamiferos (galopar(), numeroPatas())
+     * - humano : IMamiferos (pensar(), numeroPatas())
+     * - gorila : IMamiferos (trepar(), numeroPatas())
+     * 
+     * new clase -> lagartija
+     * 
+     * IMamiferos
+     * - numeroPatas() Que hacemos para agregarlo? Utilizamos clases abstractas
+     * 
+     * CLASE ABSTRACTA
+     * Es la que menos cosas es capaz de hacer y es aquella que se encuentra en la cima de la jerarquia
+     * 
+     */
+}
