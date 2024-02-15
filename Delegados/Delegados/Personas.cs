3a985399ac94f6c4d91362e72212bfce1873a0a8
Sonny Mayta <mayta.sonny@gmail.com>
2024-02-15 09:16:01 -0400

    Delegados predicados

---
+++ Delegados/Delegados/Personas.cs
@@ -0,0 +1,12 @@
+﻿namespace Delegados
+{
+    internal class Personas
+    {
+        private string _nombre;
+        private int _edad;
+
+        public string Nombre { get => _nombre; set => _nombre = value; }
+
+        public int Edad { get => _edad; set => _edad = value; }
+    }
+}
--- Delegados/Delegados/Program.cs
+++ Delegados/Delegados/Program.cs
@@ -4,6 +4,39 @@
     {
         static void Main(string[] args)
         {
+            List<Personas> list = new List<Personas>();
+            Personas P1 = new Personas();
+            P1.Nombre = "Juan";
+            P1.Edad = 18;
+
+            Personas P2 = new Personas();
+            P2.Nombre = "Maria";
+            P2.Edad = 28;
+
+            Personas P3 = new Personas();
+            P3.Nombre = "Ana";
+            P3.Edad = 46;
+
+            list.AddRange(new Personas[] { P1, P2, P3 });
+            Predicate<Personas> predicadoPersonas = new Predicate<Personas>(ExisteJuan);
+            bool existe = list.Exists(predicadoPersonas);
+            if(existe) { Console.WriteLine("Hay personas que se llaman Juan"); } else { Console.WriteLine("No hay nadie llamado Juan"); }
+
+            List<int> listaNumeros = new List<int>();
+            listaNumeros.AddRange(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 });
+
+            // Declaramos delegado predicado
+            Predicate<int> predicate = new Predicate<int>(DamePares);
+            listaNumeros.FindAll(predicate);
+
+            List<int> numerosPares = listaNumeros.FindAll(predicate);
+
+            foreach (int numero in numerosPares)
+            {
+                Console.WriteLine(numero);
+            }
+           
+
             // La creación del objeto delegado apuntando a MensajeBienvenida
             ObjetoDelegado objetoDelegado = new ObjetoDelegado(MensajeBienvenida.SaludoBienvenida);
             
@@ -17,6 +50,23 @@
         // Creando un delegado
         // Debe tener la misma structura que los metodos que llama
         delegate void ObjetoDelegado(string text);
+
+        static bool DamePares(int num)
+        {
+            if (num % 2 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }           
+        }
+
+        static bool ExisteJuan(Personas persona)
+        {
+            if (persona.Nombre == "Juan") { return true; } else { return false; }
+        }
     }
 
     /*
@@ -32,5 +82,11 @@
      * delegate tipo NombreDelegado(argumentos);
      * - Podrá llamar solo a métodos con la misma estructura
      * tipo nombreMetodo(argumentos);
+     * DELEGADOS PREDICADOS
+     * - Son delegados que devulven true o false
+     * - Sirven para muchas tareas diferentes. Muy utilizados para filtrar listas de valores comprobando
+     * si una condición es cierta para un valor dado
+     * Sintaxis:
+     * Predicate<T> nombrePredicado = new Predicate<T>(funcionDelegado)
      */
 }
