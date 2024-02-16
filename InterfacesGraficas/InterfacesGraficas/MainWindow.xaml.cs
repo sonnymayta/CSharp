86fbd787d0cd0343f18cdaa24796b5f277121fad
Sonny Mayta <mayta.sonny@gmail.com>
2024-02-16 09:03:57 -0400

    Interfaces graficas propiedades, stackpanel y eventos de botón

--- InterfacesGraficas/InterfacesGraficas/MainWindow.xaml.cs
+++ InterfacesGraficas/InterfacesGraficas/MainWindow.xaml.cs
@@ -20,24 +20,31 @@ namespace InterfacesGraficas
         {
             InitializeComponent();
 
-            Grid grid = new Grid();
-            Content = grid;
-            Button button = new Button();
-            //button.Width = 120;
+            //Grid grid = new Grid();
+            //Content = grid;
+            //Button button = new Button();
+            //button.Width = 200;
             //button.Height = 50;
-            //button.Content = "Hola";
-            WrapPanel wrapPanel = new WrapPanel();
-            TextBox textBox1 = new TextBox();
-            textBox1.Text = "Click";
-            wrapPanel.Children.Add(textBox1);
-            TextBox textBox2 = new TextBox();
-            textBox2.Text = "Click";
-            wrapPanel.Children.Add(textBox2);
-            TextBox textBox3 = new TextBox();
-            textBox3.Text = "Click";
-            wrapPanel.Children.Add(textBox3);
-            button.Content = wrapPanel;
-            grid.Children.Add(button);
+            //button.Background = Brushes.Blue;
+            //WrapPanel wrapPanel = new WrapPanel();            
+            //TextBlock textBlock1 = new TextBlock();
+            //textBlock1.Text = "Hola";
+            //wrapPanel.Children.Add(textBlock1);
+            //TextBlock textBlock2 = new TextBlock();
+            //textBlock2.Text = "Click";
+            //wrapPanel.Children.Add(textBlock2);
+            //TextBlock textBlock3 = new TextBlock();
+            //textBlock3.Background = Brushes.Red;
+            //textBlock3.Foreground = Brushes.Green;
+            //textBlock3.Text = "Enviar";
+            //wrapPanel.Children.Add(textBlock3);
+            //button.Content = wrapPanel;
+            //grid.Children.Add(button);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Le has dado al botón de nuevo.");
         }
     }
 }
